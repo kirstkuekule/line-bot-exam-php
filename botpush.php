2c70eeaeb25c187c34ece32e3f6b2864f492bca3
kirstkuekule <vijitnamom@gmail.com>
2018-09-03 09:29:46 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,9 @@
 
 require "vendor/autoload.php";
 
-$access_token = 'Xk666yclu4KhQdp+3QIv8PNf2fNAN2z12dKJyZaqNgCjT7xejglCz1YFxdSb52IdKD62vPkKJdI2FiPWP6Rvb+ePo4+GYuhEliV4pYI0mB5jJf0EjVJhPIxD2k9z6GpWrFleBFfZYTEQNHGHV4GiVgdB04t89/1O/w1cDnyilFU=';
+$access_token = '53y6fgeI+vPQDMSiO7CmuuwWKkT0tCFxMYiG7ni0BdQkqmy02xHxM/stMRC3lh77tSa3Jcocg11orB1sEmF8dsZ82Mg7jUc85+GXyz5K4JrVT3OYVQ0n0UxNdzo6/b/XdlFyinms5VqJ4jAHNRXYjQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '724f47bb58a742878692026c421b345a';
+$channelSecret = '36a850d2b65d5c69838bf4f2abaad5e5';
 
 $pushID = 'U92763d00fc1a27f8c121cbae0c17643c';
 
