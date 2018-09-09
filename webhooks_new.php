9f4a1d6db0a8d6b2a736d2b43efa5e8df2cca283
kirstkuekule <vijitnamom@gmail.com>
2018-09-09 13:15:14 +0700

    Update webhooks_new.php

--- webhooks_new.php
+++ webhooks_new.php
@@ -33,29 +33,32 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
         $arrPostData['messages'][0]['packageId'] = "2";
         $arrPostData['messages'][0]['stickerId'] = "46";
        
-}else if($message == "รูปน้องแมว"){
-        $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "image";
-        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
-        $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
-        replyMsg($arrayHeader,$arrayPostData);
- }else if($message == "พิกัดสยามพารากอน"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "location";
-        $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
-        $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
-        $arrayPostData['messages'][0]['latitude'] = "13.7465354";
-        $arrayPostData['messages'][0]['longitude'] = "100.532752";
-        replyMsg($arrayHeader,$arrayPostData);
- }else if($message == "ลาก่อน"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
-        $arrayPostData['messages'][1]['type'] = "sticker";
-        $arrayPostData['messages'][1]['packageId'] = "1";
-        $arrayPostData['messages'][1]['stickerId'] = "131";
-        replyMsg($arrayHeader,$arrayPostData);
+}else if($arrJson['events'][0]['message']['text'] == "รูปน้องแมว"){
+  $arrPostData = array();
+  $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "image";
+  $arrPostData['messages'][0]['originalContentUrl'] = $image_url;
+  $arrPostData['messages'][0]['previewImageUrl'] = $image_url;
+   
+ }else if($arrJson['events'][0]['message']['text'] == "พิกัดสยามพารากอน"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "location";
+  $arrPostData['messages'][0]['title'] = "สยามพารากอน";
+  $arrPostData['messages'][0]['address'] =   "13.7465354,100.532752";
+  $arrPostData['messages'][0]['latitude'] = "13.7465354";
+  $arrPostData['messages'][0]['longitude'] = "100.532752";
+  
+ }else if($arrJson['events'][0]['message']['text'] == "ลาก่อน"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
+  $arrPostData['messages'][1]['type'] = "sticker";
+  $arrPostData['messages'][1]['packageId'] = "1";
+  $arrPostData['messages'][1]['stickerId'] = "131";
+     
  }else if($arrJson['events'][0]['message']['text'] == "เพลง"){
         $arrPostData = array();
         $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
